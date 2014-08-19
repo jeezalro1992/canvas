ed0ecf4dc4752921cfea411b19a1f22ca2f27521
jeezalro <jesus.alberto.lopez@outlook.com>
2014-08-18 23:27:25 -0600

    canvas

--- js/canvas.js
+++ js/canvas.js
@@ -9,19 +9,21 @@ function dibujo(){
 	con.strokeStyle="#edba1c";
 	con.stroke();
 	con.closePath();
+
 	con.beginPath();
 	con.arc(120,120,10,0, Math.PI*2, true);
 	con.arc(180,120,10,0, Math.PI*2, true);
 	con.fillStyle="#000";
 	con.fill();
 	con.closePath();
+
 	con.beginPath();
 	con.arc(125,115,3,0, Math.PI*2, true);
 	con.arc(185,115,3,0, Math.PI*2, true);
-
 	con.fillStyle="#fff";
 	con.fill();
 	con.closePath();
+
 	con.beginPath();
 	con.moveTo(90,160);
 	con.arc(150,160,60,0,Math.PI, false);
@@ -30,12 +32,13 @@ function dibujo(){
 	con.strokeStyle="#ff003e"
 	con.stroke();
 	con.closePath();
+
 	con.beginPath();
-	
 	con.arc(150,190,20,0,Math.PI*1, true);
 	con.fillStyle="#000";
 	con.fill();
 	con.closePath();
+	
 	con.beginPath();
 	con.arc(150,190,30,0,Math.PI, false);
 	con.arc(135,190,15,0,Math.PI, true);
